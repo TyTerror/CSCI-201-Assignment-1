fda9e091233b8dbd0ae4a7170ee944903ae8cb1e
Tyler Ramsey <tyler.elias.ramsey@gmail.com>
2018-10-16 15:10:53 -0400

    Update main.asm
    "Ty" --> "Tyler"

--- main.asm
+++ main.asm
@@ -4,4 +4,11 @@
     la $a0, 84        # T
     syscall
     addi $a0, $a0, 37 # y (121)
-                      # Gonna have to wrap around
+    syscall
+                      # Gonna have to wrap around(i think) unless i subtract
+    sub $a0, $a0, 13  #l (108)
+    syscall
+    sub $a0, $a0, 07 # e (101)
+    syscall
+    addi $a0, $a0, 13 # r (114)
+    syscall
