0704f7290e34f09c398f1a4ac2bbf34001edfd80
Tyler Ramsey <tyler.elias.ramsey@gmail.com>
2018-10-18 23:25:10 -0400

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,31 +1,29 @@
 .text
   main:
-    li $v0, 11
-    la $a0, 82         # R          ------------Last Name Start----------------
-    syscall
-    addi $a0, $a0, 15  # a (97)
-    syscall
-    addi $a0, $a0, 12  # m (109)
-    syscall
-    addi $a0, $a0, 06  # s (115)
-    syscall
-    sub $a0, $a0, 14   # e (101)
-    syscall
-    addi $a0, $a0, 20   # y (121)   -------------Last Name End-----------------
-    syscall
-    sub $a0, $a0, 77   # , (44)
-    syscall
-    sub $a0, $a0, 12   # SPACE (32)
-    syscall
     #li $v0, 11
-    #la $a0, 84        # T          ------------First Name Start----------------
+    #la $a0, 82         # R          ------------Last Name Start----------------
+    #syscall
+    #addi $a0, $a0, 15  # a (97)
+    #syscall
+    #addi $a0, $a0, 12  # m (109)
+    #syscall
+    #addi $a0, $a0, 06  # s (115)
+    #syscall
+    #sub $a0, $a0, 14   # e (101)
+    #syscall
+    #addi $a0, $a0, 20   # y (121)   -------------Last Name End-----------------
+    #syscall
+    #sub $a0, $a0, 77   # , (44)
+    #syscall
+    #sub $a0, $a0, 12   # SPACE (32)
+    #syscall
+    #addi $a0, $a0, 52  # T  (84)        ------------First Name Start----------------
     #syscall
     #addi $a0, $a0, 37 # y (121)
     #syscall
-                      # Gonna have to wrap around(i think) unless i subtract
     #sub $a0, $a0, 13  #l (108)
     #syscall
     #sub $a0, $a0, 07 # e (101)
     #syscall
     #addi $a0, $a0, 13 # r (114)   -------------First Name End-----------------
-    #syscall
+    syscall
